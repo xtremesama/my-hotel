7aeaec4ec0f893f673f6e457aa68f7d17f55f891
Archie Torres <xtremesama@gmail.com>
2022-04-29 22:02:15 -0700

    Added a small change in link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or go home
         </a>
       </header>
     </div>
